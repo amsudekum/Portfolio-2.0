4ec9a47f9f4146198c5db814a93b566d0a69f3d4
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-09 20:40:55 -0500

    file structure stuff

---
+++ portfolio/src/components/header.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import '../styles/Header.css';
+
+function Header(){
+    return(
+    <div>
+        <ul>
+            <li><a href="#aboutme">.About</a></li>
+            <li><a href="#contactme">Contact.</a></li>
+        </ul>
+        <h2>.ASHLEY SUDEKUM.</h2>
+        <ul>
+            <li><a href="#mywork">.Work</a></li>
+            <li><a href="">Resume.</a></li>
+        </ul>
+    </div>
+    )
+}
