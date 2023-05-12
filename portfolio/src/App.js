8df228e08cded02cb0a56c58606a339e870ce71d
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-11 20:53:29 -0500

    tailwind commits

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-
+import About from "./components/about";
+import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Header from "./components/header";
 export default function App() {
   return (
     <main>
+      <Header />  
       <Navbar />
       <About />
       <Projects />
