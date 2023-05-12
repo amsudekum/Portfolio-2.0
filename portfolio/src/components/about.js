969646748b82f3d15f5c55d2ed92eb9e3ecdba68
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-11 21:06:13 -0500

    brought over my about me from my original portfolio

---
+++ portfolio/src/components/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div id="aboutme" class="aboutme">
+            <img src="assets/images/Ashley Profile Image.JPG" alt="Ashley in a pool of sprinkles." className="aboutme-sprinkles"/>
+            <div class="aboutme-pre">
+            Hi, I'm Ashley. 
+            I'm a Kansas City native who's decided it's time for a new adventure. 
+            After six years behind the chair in a high end salon, I was ready for something 
+            new. I'm hoping to blend those two worlds together to achieve a fulfilling 
+            career. 
+            When I'm not camped out at my desk, I'm spending quality time with my husband
+            Sam and our cat Shortcake.
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
